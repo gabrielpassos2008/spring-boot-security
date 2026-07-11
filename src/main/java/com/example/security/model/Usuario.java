57849b9825afcc3a849e0972202c2bb8199ed2f0
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-11 14:27:18 -0300

    - ajuste no nome da classe aonde precisa ser com letra mauiscula. - criando meu repository para o Usuario usando o UserDetails

--- src/main/java/com/example/security/model/Usuario.java
+++ src/main/java/com/example/security/model/Usuario.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class usuario implements UserDetails {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/example/security/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.example.security.repository;
+
+import com.example.security.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<UserDetails> findUserByEmail(String username);
+}
