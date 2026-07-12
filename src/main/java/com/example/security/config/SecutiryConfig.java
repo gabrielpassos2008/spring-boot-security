0526cc48ce99279a5dd395e016b6b9023669e7fb
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-12 20:22:25 -0300

    - adicionando o SecurityFilter no SecurityConfig. -criando um controller para testar

--- src/main/java/com/example/security/config/SecutiryConfig.java
+++ src/main/java/com/example/security/config/SecutiryConfig.java
@@ -1,6 +1,7 @@
 package com.example.security.config;
 
 import jakarta.servlet.DispatcherType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecutiryConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -29,6 +34,7 @@ public class SecutiryConfig {
                         .requestMatchers(HttpMethod.POST,"/auth/registar").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
