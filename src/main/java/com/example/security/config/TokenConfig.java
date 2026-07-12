bbce11beb8b7426f69f0a5b9d80dde9577f3b04c
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-12 20:20:26 -0300

    - criando o metedo que vaio fazer a decodificaçao do token

--- src/main/java/com/example/security/config/TokenConfig.java
+++ src/main/java/com/example/security/config/TokenConfig.java
@@ -3,10 +3,13 @@ package com.example.security.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.security.model.Usuario;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenConfig {
@@ -24,4 +27,20 @@ public class TokenConfig {
                 .withIssuedAt(Instant.now())
                 .sign(algorithm);
     }
+
+    public Optional<JWTUserData> validarToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            DecodedJWT decodedJWT = JWT.require(algorithm).build().verify(token);
+            return Optional.of(JWTUserData.builder()
+                    .idUsuario(decodedJWT.getClaim("idUsuario").asLong())
+                    .email(decodedJWT.getSubject())
+                    .build());
+
+        }catch (JWTVerificationException exception){
+            return Optional.empty();
+        }
+
+        }
+
 }
